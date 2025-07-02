a7851c33e5e21c8e5740d7d1c7c93a997c0b1298
sanjogbhalla16 <sanjog.bhalla@gmail.com>
2025-07-02 13:36:16 +0530

    changes in header section

--- my-portfolio/src/components/Header.tsx
+++ my-portfolio/src/components/Header.tsx
@@ -1,16 +1,28 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
+import Nav from "./Nav";
 
 const Header = () => {
   return (
     <header className="py-8 xl:py-12 text-white">
-      <div className="container mx-auto">
-        {/*Logo*/}
+      <div className="container mx-auto flex justify-between items-center">
+        {/* Logo */}
         <Link href="/">
           <h1 className="text-4xl font-semibold">
             Sanjog<span className="text-accent">.</span>
           </h1>
         </Link>
+
+        {/* Desktop Nav & Hire Me Button */}
+        <div className="hidden xl:flex items-center gap-8">
+          <Nav />
+          <Link href="/contact">
+            <Button variant="accent">Hire me</Button>
+          </Link>
+        </div>
+
+        {/* Mobile Nav */}
+        <div className="xl:hidden">mobile nav</div>
       </div>
     </header>
   );
