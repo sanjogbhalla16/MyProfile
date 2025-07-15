052646c1bf26b4dc39f614aa1f194734caf58682
sanjogbhalla16 <sanjog.bhalla@gmail.com>
2025-07-15 08:42:09 +0530

    Made the stair transition

--- my-portfolio/src/components/StairTransition.tsx
+++ my-portfolio/src/components/StairTransition.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { usePathname } from "next/navigation";
-import { AnimatePresence } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import Stairs from "./Stairs";
 
 const StairTransition = () => {
@@ -13,6 +13,14 @@ const StairTransition = () => {
           <div className="h-screen w-screen fixed top-0 left-0 right-0 z-40 pointer-events-none flex">
             <Stairs />
           </div>
+          <motion.div
+            className="h-screen w-screen fixed bg-primary top-0 pointer-events-none"
+            initial={{ opacity: 1 }}
+            animate={{
+              opacity: 0,
+              transition: { delay: 1, duration: 0.4, ease: "easeInOut" },
+            }}
+          ></motion.div>
         </div>
       </AnimatePresence>
       ;
