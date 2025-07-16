c018c44a8f1f002cfdf8be690699a703c5d0880c
sanjogbhalla16 <sanjog.bhalla@gmail.com>
2025-07-16 09:00:55 +0530

    Making of Mobile Nav

--- my-portfolio/src/components/Header.tsx
+++ my-portfolio/src/components/Header.tsx
@@ -1,6 +1,7 @@
 import Link from "next/link";
 import { Button } from "./ui/button";
 import Nav from "./Nav";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
--- my-portfolio/src/components/MobileNav.tsx
+++ my-portfolio/src/components/MobileNav.tsx
@@ -1,7 +1,39 @@
-import React from "react";
+"use client";
+
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import { usePathname } from "next/navigation";
+import Link from "next/link";
+import { CiMenuFries } from "react-icons/ci";
+
+const links = [
+  {
+    name: "home",
+    path: "/",
+  },
+  {
+    name: "resume",
+    path: "/resume",
+  },
+  {
+    name: "work",
+    path: "/work",
+  },
+  {
+    name: "contact",
+    path: "/contact",
+  },
+];
 
 const MobileNav = () => {
-  return <div>MobileNav</div>;
+  const pathname = usePathname();
+  return (
+    <Sheet>
+      <SheetTrigger className="flex justify-center items-center">
+        <CiMenuFries className="text-{32px} text-accent"></CiMenuFries>
+      </SheetTrigger>
+      <SheetContent className="flex flex-col">content</SheetContent>
+    </Sheet>
+  );
 };
 
 export default MobileNav;
