c70899f61856d678d6d40fcd384607df6a703cf7
sanjogbhalla16 <sanjog.bhalla@gmail.com>
2025-07-21 16:53:46 +0530

    Making our homepage

--- my-portfolio/src/app/page.tsx
+++ my-portfolio/src/app/page.tsx
@@ -4,7 +4,36 @@ import { FiDownload } from "react-icons/fi";
 const Home = () => {
   return (
     <section className="h-full">
-      <div className="container mx-auto">homepage </div>
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-8 xl:pb-24">
+          {/* text */}
+          <div className="text-center xl:text-left">
+            <span className="text-xl">Software Developer</span>
+            <h1 className="h1 mb-6">
+              Hello I'm <br />{" "}
+              <span className="text-accent">Sanjog Bhalla</span>
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white/80">
+              I excel at crafting elegant digital experience and I am proficient
+              in various programming languages and technologies.{" "}
+            </p>
+            {/* btn and socials*/}
+            <div className="flex flex-col xl:flex-row items-center gap-8">
+              <Button
+                variant="outline"
+                size="lg"
+                className="uppercase flex items-center gap-2"
+              >
+                <span>Download CV</span>
+                <FiDownload className="text-xl"></FiDownload>
+              </Button>
+              <div>socials</div>
+            </div>
+          </div>
+          {/* photo */}
+          <div>photo</div>
+        </div>
+      </div>
     </section>
   );
 };
